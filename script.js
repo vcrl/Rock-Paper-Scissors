e9730c5fe11ac98b49cb0f560b6a8afa76983007
vcrl <vincentruelle.pro@gmail.com>
2022-11-08 12:00:24 +0100

    Converted floating functions to Game() class

--- script.js
+++ script.js
@@ -1,87 +1,73 @@
-const possibleChoices = ["paper", "scissors", "rock"];
-let playBtn = document.getElementById("play");
+class Game {
+    possibleChoices = ["paper", "scissors", "rock"];
+    playerBtn = document.getElementById("play");
 
-function getComputerChoice(){
-    /* 
-        Generates the computer's choice 
-    */
+    playRound(){
+        /* 
+            Launches a round 
+        */
 
-    return Math.floor(Math.random() * possibleChoices.length);
-}
-
-function getPlayerChoice(){
-    /* 
-        Gets player input from a prompt and determine his choice 
-    */
-
-    let playerSelection = prompt("Please, select Rock, Paper or Scissors");
-    playerSelection = playerSelection.toLowerCase();
-    switch(playerSelection){
-        case "paper":
-            console.log("You have choosen Paper.")
-            return 0;
-        case "scissors":
-            console.log("You have choosen Scissors.")
-            return 1;
-        case "rock":
-            console.log("You have choosen Rock.")
-            return 2;
-        default:
-            console.log("You have to choose between Rock, Paper or Scissors.");
-            playRound();
-            return;
-    }
-}
-
-function playRound(){
-    /* 
-        Launches a round 
-    */
-
-    // Define playerSelection && checks if not null
-    let playerSelection = possibleChoices[getPlayerChoice()];
-    // If playerSelection != null
-    if(playerSelection){
-        let computerSelection = possibleChoices[getComputerChoice()];
-        // Format choices
-        playerSelection = formatString(playerSelection);
-        computerSelection = formatString(computerSelection);
-        // Display computer choice
-        console.log(`Computer chose ${computerSelection}.`);
-        // Verify if Win, Lose or Tie
-        if(playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper"){
-            return console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-        }else if(playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"){
-            return console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        let playerSelection = this.possibleChoices[this.getPlayerChoice()];
+        if(playerSelection){
+            let computerSelection = this.possibleChoices[this.getComputerChoice()];
+            playerSelection = this.formatString(playerSelection);
+            computerSelection = this.formatString(computerSelection);
+            console.log(`Computer chose ${computerSelection}.`);
+            if(playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper"){
+                return console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+            }else if(playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"){
+                return console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+            }else{
+                return console.log(`It's a tie! You both picked ${computerSelection}.`);
+            }
         }else{
-            return console.log(`It's a tie! You both picked ${computerSelection}.`);
+            return;
         }
-    // If playerSelection == null, abort
-    }else{
-        return;
     }
-}
+    getComputerChoice(){
+        /* 
+            Generates the computer's choice 
+        */
+    
+        return Math.floor(Math.random() * this.possibleChoices.length);
+    }
+    
+    getPlayerChoice(){
+        /* 
+            Gets player input from a prompt and determine his choice 
+        */
+    
+        let playerSelection = prompt("Please, select Rock, Paper or Scissors");
+        playerSelection = playerSelection.toLowerCase();
+        switch(playerSelection){
+            case "paper":
+                console.log("You have choosen Paper.")
+                return 0;
+            case "scissors":
+                console.log("You have choosen Scissors.")
+                return 1;
+            case "rock":
+                console.log("You have choosen Rock.")
+                return 2;
+            default:
+                console.log("You have to choose between Rock, Paper or Scissors.");
+                this.playRound();
+                return;
+        }
+    }
 
-function formatString(stringElement){
+    formatString(stringElement){
         /* 
             Formats a string to return said-string with the first letter capitalized 
         */
 
-    switch(typeof stringElement){
-        case "string":
-            let unformattedStringCap = stringElement[0].toUpperCase();
-            string = unformattedStringCap + stringElement.slice(1);
-            return string;
-        default:
-            return;
+        switch(typeof stringElement){
+            case "string":
+                let unformattedStringCap = stringElement[0].toUpperCase();
+                let string = unformattedStringCap + stringElement.slice(1);
+                return string;
+            default:
+                return;
+        }
     }
 }
-
-/*
-class Game {
-    constructor(){
-        this.possibleChoices = ["paper", "scissors", "rock"];
-        this.playerBtn = document.getElementById("play");
-    }
-}
-*/
