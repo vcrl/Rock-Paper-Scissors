0a80376ac67666fc29b7fdddc1d41b153c910acd
vcrl <vincentruelle.pro@gmail.com>
2022-11-10 10:23:06 +0100

    Added a 5 round algorith + added new text styles to the status text

--- script.js
+++ script.js
@@ -23,30 +23,50 @@ class Game {
         /* 
             Launches a round 
         */
-       
+
         let playerSelection = this.possibleChoices[choice];
         this.playerWeapon.innerHTML = this.choicesEmojis[choice];
-        let computerSelection = this.possibleChoices[this.getComputerChoice()];
+        let computerSelection = this.possibleChoices[this.#getComputerChoice()];
         this.computerWeapon.innerHTML = this.choicesEmojis[this.possibleChoices.indexOf(computerSelection)];
 
         if(playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock" || playerSelection == "Rock" && computerSelection == "Paper"){
-            this.secondText.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}.`;
-            this.addScorePoint("Computer", 1);
-            this.computerScore.innerHTML = "Computer: " + this.computerScoreCount;
+            if(this.computerScoreCount + 1 !== 5){
+                this.secondText.style = "color:red;"
+                this.secondText.innerHTML = `💥 Ouch! ${computerSelection} beats ${playerSelection}.`;
+                this.#updateScoreDisplay("Computer", true);
+                this.#updateScoreDisplay("Player", false);
+            }else{
+                this.#updateScoreDisplay("Computer", true);
+                this.secondText.style = "color:red;"
+                this.secondText.innerHTML = "❌ Computer won the game by having 5 points ! ❌";
+                this.#resetScorePoints();
+            }
+            
             return;
         }else if(playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Rock"){
-            this.secondText.innerHTML = `You win! ${playerSelection} beats ${computerSelection}.`;
-            this.addScorePoint("Player", 1);
-            this.playerScore.innerHTML = "Player: " + this.playerScoreCount;
-            return;
+            if(this.playerScoreCount + 1 !== 5){
+                this.secondText.style = "color:lime;"
+                this.secondText.innerHTML = `🥊 You got this! ${playerSelection} beats ${computerSelection}.`;
+                this.#updateScoreDisplay("Player", true);
+                this.#updateScoreDisplay("Computer", false);
+                return;
+            }else{
+                this.#updateScoreDisplay("Player", true);
+                this.secondText.style = "color:lime;"
+                this.secondText.innerHTML = "🏆 You won the game by having 5 points 🏆";
+                this.#resetScorePoints();
+            }   
         }else{
+            this.#updateScoreDisplay("Player", false);
+            this.#updateScoreDisplay("Computer", false);
+            this.secondText.style = "color:#f7fcff;"
             this.secondText.innerHTML = `It's a tie! You both picked ${computerSelection}.`;
             return;
         }
     }
 
 
-    getComputerChoice(){
+    #getComputerChoice(){
         /* 
             Generates the computer's choice 
         */
@@ -54,11 +74,28 @@ class Game {
         return Math.floor(Math.random() * this.possibleChoices.length);
     }
 
-    endGame(){
+    #resetScorePoints(){
+        this.computerScoreCount = 0;
+        this.playerScoreCount = 0;
         return;
     }
 
-    addScorePoint(player, amount){
+    #updateScoreDisplay(player, addPoint = false){
+        switch(player){
+            case "Player":
+                addPoint ? this.#addScorePoint("Player", 1) : null;
+                this.playerScore.innerHTML = "Player: " + this.playerScoreCount;
+                break;
+            case "Computer":
+                addPoint ? this.#addScorePoint("Computer", 1) : null;
+                this.computerScore.innerHTML = "Computer: " + this.computerScoreCount;
+                break;
+            default:
+                break;
+        }
+    }
+
+    #addScorePoint(player, amount){
         switch(player){
             case "Player":
                 this.playerScoreCount = this.playerScoreCount + amount;
@@ -68,6 +105,7 @@ class Game {
                 break;
         }
     }
+
 
 }
 
